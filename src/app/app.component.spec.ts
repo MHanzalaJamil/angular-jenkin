afe292298bb1908a01c97ad88744c4d9ee156a79
MHanzalaJamil <muhammadhanzalaj@gmail.com>
2025-07-02 00:54:40 -0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,18 +12,20 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-jenkin'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-jenkin');
   });
 
+  it('should have h1 as Hello World', () => {
+    pending();
+  })
 });
